f7483a3a801a41b9c2c998a762f5500347aaea8c
Shubhangi Sinha <sinhashubhi297@gmail.com>
2023-12-25 23:06:18 +0530

    displayed service acount

---
+++ server/config/serviceAccount.js
@@ -0,0 +1,14 @@
+export const CREDENTIALS= JSON.parse(JSON.stringify({
+    "type": "service_account",
+    "project_id": "quixotic-booth-409015",
+    "private_key_id": "5cc3cd785c22d974e442998d9990f89797dc296e",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCiPyh8bFPenxyx\nUjIlxJ/ODidPH37wRaLhZNvq6YQBSounHLsgDLOkwRFmmj1tzwogvpu1mqKhqkG2\nTFq/0/J+mP4VcGvOLTEf2O2Xfo9pwfOnLB/r3E3Jrh46d4HzJ8la7h88rtPGWWJE\nmb1fNLttOQfY7Jxn3FKzzrIaqYZo/UoBpvqfkvdYB8axzdj2eU+vQ3WfOLhmWVMx\nNsGvXuS9EQCm0H66EtOLbN5lZX7IS5op09TE5EyCCUbjWzzWYTHn3jdzwpkbui38\nAZbmngHRRO9JegASTVj9twhtadsjVh/CdP1Q1bvCcY52G4dtSgnHOin4irlRK7Yc\nfn8435AvAgMBAAECggEABF8M9w04sK7NVfUiHeSq29gVxwrRDUp3lYsH1BkB9p3M\nsNvvl96bKr0yDkB74FqnF8Yve8rP63si1YetrtH8wyWQqcoRCzdOxc4IIduhWq3N\n7QjUc1d/l+LjWQkPPz8EUTkknAXwrHP5xT0alQ4Vgzqwaks1sCRi7OVDm7+3zjVw\nqhy6mtmSU5tD+hOKWpWR9JoDO9k6ylj1XjmywZd6oVe1b5jlNiEhdgrVYxPlSpYT\nSneHSFAZzv5cCwNakLSGON17MwDs6l+AQ/C4qI5R4DwdWn9cMLUigUtZtxUN2kCe\nZnfAeSD+PhpZa/N3NKoWXpiKrnI5jWt0IM6Ec7TMwQKBgQDX4m17TdDDUa7sQLeJ\nEStixClrXX+qcjnE8cF9g0bqvSkTChUr8bET+simh3nCiKyAbBCqm8PHf7Q35YPm\n12ldm5eRcKxwLgbfGpj3pP89nvf8Mpzi7eQHbGMO8rK/REFZu5eTxxCdS5AG4JP0\nWkUoJE6a77LIWNQRV0tLA/nUMwKBgQDAZTHarcbrsYdO+tmwGwA3/r3FeL/hMbc1\nYzfRCKRfHuIj5TePkZx2x1x4yoOf+AzEfrh445/MOVGiwkczRi7+7coeZxDejMp0\nU24WCm0JVeKXVFD1smLjRr6T4vNelOfGfZr0wrjy2gwERJ/ONXZGR8eVT+DjmarD\nooR1/zI4FQKBgDzP5qEuOVlbwBeC33B36q3Grhg1XUoJJSbKJwQRH7/uCwz28SHQ\n0l3bbfwi+3SGBXmjd0pn2E1xNrXsLCyxj1e0BY0NnTV4ca0VE/PmgZO9HQPoyfLO\nqkZBDOJgEWP5eSMqybbyA0cbyvV0+5iAeSymlZHlZYo0I4DDA02dUBmrAoGAci+d\nFxxsIxaKJ+XZrRtZVeJq3vh7aTwIcDciuX1XkNzTelpgT2UsZ5KK32XSGO8vJK/Q\n7G6sOJkMSnzjpRrSaZrXipPA/1SwE7owbARkElZW1VnFcw0cg6dJFJxttITLypJh\nlUK8YeXnm+Det5QjTjLr0UJHRs28+79Ivn9nXsECgYBguMferSXXn5b+2kPgy+Wl\nPuH9nNKTBj2XzGj+f/xIgmZs7NhmfqmSKrKZ3m3zpCDCoM0mUZHrb6Du3CNHZIvx\nlnSnGwbYQChfcKJsa3cU8p7biELypqKls3/UXJPWrZWlIcipXVGwLaWF1I7XFSLT\n8HlIP8BRHOmKobwF8SW+nA==\n-----END PRIVATE KEY-----\n",
+    "client_email": "cloud-vision-intro@quixotic-booth-409015.iam.gserviceaccount.com",
+    "client_id": "116568033337163573844",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/cloud-vision-intro%40quixotic-booth-409015.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  }));
+  
